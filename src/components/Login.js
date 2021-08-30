0120f7c8e499b6b7dc42018a9e08830ed0ef2850
vallimmateus <mateus.vallim@usp.br>
2021-08-29 23:12:56 -0300

    Teste sem sistema de login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
 import TextField from "./TextField";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Login({ toggle }) {
+	// eslint-disable-next-line
 	const [user, setUser] = useState("");
+	// eslint-disable-next-line
 	const [pass, setPass] = useState("");
 	return (
 		<form>
 			<TextField name="Usuário" required onChange={setUser} />
 			<TextField name="Senha" required onChange={setPass} />
-			{/* <Link to="/home"> */}
-			<input
-				type="button"
-				value="Login"
-				onClick={() => {
-					console.log("user", user);
-					console.log("pass", pass);
-				}}
-			/>
-			{/* </Link> */}
+			<Link to="/home">
+				<input
+					type="button"
+					value="Login"
+					// onClick={() => {
+					// 	console.log("user", user);
+					// 	console.log("pass", pass);
+					// }}
+				/>
+			</Link>
 			<input
 				type="button"
 				value="Register"
